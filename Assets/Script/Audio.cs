1ab4ab0bef8305e24a67283c80168b203194aab3
919498857 <69890230+919498857@users.noreply.github.com>
2020-09-21 00:12:52 +0800

    12

---
+++ Assets/Script/Audio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{   
+    public AudioClip audios;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > 6  && Time.time < 7) {
+            GetComponent<AudioSource>().clip = audios;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
