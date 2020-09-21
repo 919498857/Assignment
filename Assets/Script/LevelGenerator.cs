467fbb9e7bc958624288f31429e40ca47b8ea823
919498857 <69890230+919498857@users.noreply.github.com>
2020-09-21 16:56:25 +0800

    123

--- Assets/Script/LevelGenerator.cs
+++ Assets/Script/LevelGenerator.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public int a;
-    public int b;
-    public int c;
     public GameObject outsidewall;
     public GameObject insidewall;
     public GameObject outsidecorner;
     public GameObject insidecorner;
     public GameObject normalpellet;
-    public GameObject emergypellet;
+    public GameObject powerpellet;
     public GameObject life;
     public GameObject G1;
     public GameObject G2;
@@ -61,18 +58,128 @@ public class LevelGenerator : MonoBehaviour
             {
                 switch (levelMap[i, j])
                 {
-                   // case 1:
-                    
+                    case 1:
+                        if (i == 9 && i == 0)
+                        {
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 90);
+                            break;
+                        }
+                        else if (i == 9 && i == 5)
+                        {
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, -90);
+                            break;
+                        }
+                        else if (i == 13 && i == 5)
+                        {
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, -180);
+                            break;
+                        }
+                        else
+                        {
+
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                    case 2:
+                        if (i == 0)
+                        {
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                        else if (i < 9)
+                        {
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 90);
+                            break;
+                        }
+                        else if (i >= 10 & i < 13)
+                        {
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 90);
+                            break;
+                        }
+                        else
+                        {
+
+                            Instantiate(outsidecorner, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                    case 3:
+                        if ((i == 4 || i == 7) && j == 2 || (i == 4 && j == 7) || (i == 13 && j == 7) || (i == 10 && j == 13) || (i == 4 && j == 7))
+                        {
+                            Instantiate(insidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 90);
+                            break;
+                        }
+                        else if (((i == 4 || i == 7) && j == 5 || (i == 4 && j == 11) || (i == 13 && j == 8) || (i == 10 && j == 11)))
+                        {
+                            Instantiate(insidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 90);
+                            break;
+                        }
+                        else if (i >= 10 && i < 13)
+                        {
+                            Instantiate(insidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 180);
+                            break;
+                        }
+                        else
+                        {
+
+                            Instantiate(insidecorner, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 270);
+                            break;
+                        }
+                    case 4:
+                        if (i == 2 && j < 11)
+                        {
+                            Instantiate(insidewall, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                        else if (i == 4 && i < 11)
+                        {
+                            Instantiate(insidewall, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                        else if (i == 6)
+                        {
+                            Instantiate(insidewall, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                        else if (i == 7 && i < 7 || j > 8)
+                        {
+                            Instantiate(insidewall, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                        else if ((i == 9 || i == 10) && (j >= 9 && j <= 10))
+                        {
+                            Instantiate(insidewall, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                        else if (i == 12 && j > 10)
+                        {
+                            Instantiate(insidewall, new Vector2(i, j), Quaternion.identity);
+                            break;
+                        }
+                        else {
+                            Instantiate(insidewall, new Vector2(i, j), Quaternion.identity).transform.Rotate(0, 0, 90);
+                            break;
+                        }
+                    case 5:
+                        Instantiate(normalpellet, new Vector2(i, j), Quaternion.identity);
+                        break;
+                    case 6:    
+                        Instantiate(powerpellet, new Vector2(i, j), Quaternion.identity);
+                        break;
+                    case 7:
+                        Instantiate(outsidewall, new Vector2(i, j), Quaternion.identity);
+                        break;
                 }
             }
         }
-
     }
+        
+
+    
 
 
 
 // Update is called once per frame
-void Update()
+    void Update()
     {
         
     }
